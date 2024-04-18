2fcdcde13e75545ce55f889445b5c614808ad8d0
Aleksandr Semakin <ansmkn@gmail.com>
2024-04-18 19:36:21 +0300

    add adhoc lane

--- OddMusiq/Environment/Sources/Environment/Environment.swift
+++ OddMusiq/Environment/Sources/Environment/Environment.swift
@@ -1,12 +1,23 @@
 import Foundation
 
 public enum Environment: String {
+    private static let appEnvironment = Bundle.main.infoDictionary?["APP_ENVIROMENT"] as? String
+
     case unitTesting
     case sandbox
     case beta
     case production
 
-    public static var current: Environment {
-        .sandbox
-    }
+    public static let current: Environment = {
+        guard let appEnvironment else {
+            print("have no APP_ENVIRONMENT in Info.plist")
+            return .sandbox
+        }
+        guard let knownEnv = Environment(rawValue: appEnvironment) else {
+            print("Info.plist has unknown APP_ENVIRONMENT")
+            return .sandbox
+        }
+        print("Environment: \(knownEnv)")
+        return knownEnv
+    }()
 }
--- fastlane/README.md
+++ fastlane/README.md
@@ -23,6 +23,22 @@ For _fastlane_ installation instructions, see [Installing _fastlane_](https://do
 
 Push a new beta build to TestFlight
 
+### ios adhoc
+
+```sh
+[bundle exec] fastlane ios adhoc
+```
+
+Ad Hoc Distribution
+
+### ios test
+
+```sh
+[bundle exec] fastlane ios test
+```
+
+Run test
+
 ----
 
 This README.md is auto-generated and will be re-generated every time [_fastlane_](https://fastlane.tools) is run.
